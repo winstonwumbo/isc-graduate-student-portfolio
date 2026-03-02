4c68b4266be1abc81246b5808e85fd1fbce62dc5
DDEV User <nobody@example.com>
2026-03-02 16:57:53 -0500

    Page updated: Specialization (item-1c280f2b-d269-453d-9d42-22a2034c7175)

--- service-worker.js
+++ service-worker.js
@@ -48,7 +48,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-1c280f2b-d269-453d-9d42-22a2034c7175\/index.html",
-        "7NCxCnmSVlyClxmPvj4Yg"
+        "Ue7JtRkJDaVhGTWvxVzA"
     ],
     [
         "pages\/item-df2d214b-3dc2-481f-9428-383830d240ed\/index.html",
