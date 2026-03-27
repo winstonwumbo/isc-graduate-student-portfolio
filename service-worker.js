459c21edbacc7768da904e6e1ac3ea93e9506370
DDEV User <nobody@example.com>
2026-03-27 05:09:18 -0400

    Page updated: Competency (item-896b5ef1-0398-4efe-a522-cfced24f947f)

--- service-worker.js
+++ service-worker.js
@@ -48,7 +48,7 @@ var precacheConfig = [
     ],
     [
         "pages\/item-896b5ef1-0398-4efe-a522-cfced24f947f\/index.html",
-        "R2oEXuiAQfbjrIzLbAug"
+        "Qo9ml4rAauVRWOihYphVtA"
     ],
     [
         "pages\/item-a085aae3-18ed-4b26-a2a0-8c7e0fd67974\/index.html",
